7a56e844a5c398df1df1f16093a22646942010d0
shrivarshan-07 <shri31007@gmail.com>
2026-08-08 13:59:03 +0530

    Final Commit

---
+++ 2Cclient.py
@@ -0,0 +1,7 @@
+import socket
+s=socket.socket()
+s.connect(('localhost',8000))
+while True:
+    ip=input("Enter logical Address : ")
+    s.send(ip.encode())
+    print("MAC Address",s.recv(1024).decode())
